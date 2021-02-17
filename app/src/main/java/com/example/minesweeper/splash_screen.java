2694b5a4e6d115c9937320af7b4f239a8aa83252
Adarsh Goswami <adarsh010120@gmail.com>
2021-02-17 19:39:53 +0530

    design home page

--- app/src/main/java/com/example/minesweeper/splash_screen.java
+++ app/src/main/java/com/example/minesweeper/splash_screen.java
@@ -5,15 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class splash_screen extends AppCompatActivity {
 
-    public static final int SPLASH_SCREEN= 3000;
+    public static final int SPLASH_SCREEN= 6000;
+    private LottieAnimationView nothing, explosion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        explosion= findViewById(R.id.lottie_explosion);
+        nothing= findViewById(R.id.lottie_nothing);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                nothing.setVisibility(View.GONE);
+                explosion.setVisibility(View.VISIBLE);
+            }
+        }, SPLASH_SCREEN);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -21,6 +36,6 @@ public class splash_screen extends AppCompatActivity {
                 Intent intent= new Intent(splash_screen.this, MainActivity.class);
                 startActivity(intent);
             }
-        }, SPLASH_SCREEN);
+        }, SPLASH_SCREEN+500);
     }
 }
