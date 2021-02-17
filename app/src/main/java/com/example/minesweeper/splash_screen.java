2c50e8104f8a29389d178aeb03c270a611ce28cc
Adarsh Goswami <adarsh010120@gmail.com>
2021-02-17 08:22:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/minesweeper/splash_screen.java
@@ -0,0 +1,26 @@
+package com.example.minesweeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    public static final int SPLASH_SCREEN= 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(splash_screen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_SCREEN);
+    }
+}
